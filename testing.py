0c4d713604800560da58b999bbfddf4f3a93fe19
Jack A. Botelho <35936122+jbotelho97@users.noreply.github.com>
2019-02-21 14:06:43 -0500

    Add files via upload

--- testing.py
+++ testing.py
@@ -2,42 +2,36 @@
 
 import sys
 from random import randint
-from convexhull import computeHull, quickSort, naiveHull
+from convexhull import computeHull
 
 
 # argv takes a number randomly generates that many points
 def main():
-    # if len(sys.argv) != 2:
-    #     exit(1)
-    #
-    # numGen = int(sys.argv[1])
+    if len(sys.argv) != 2:
+        exit(1)
+
+    numGen = int(sys.argv[1])
 
     points = []
 
-    points.append([0, 0])
-    points.append([0, 1])
-    points.append([1, 0])
-    points.append([2, 0])
-    points.append([3, 0])
-    points.append([1, 1])
 
-    numGen = len(points)
+    for i in range(numGen):
+        randX = randint(0, 2*numGen)
+        randY = randint(0, 2*numGen)
+        temp = (randX, randY)
+        points.append(temp)
 
-    # for i in range(numGen):
-    #     randX = randint(0, 10000000)
-    #     randY = randint(0, 10000000)
-    #     temp = (randX, randY)
-    #     points.append(temp)
-
-    # answers = naiveHull(points)
     answers = computeHull(points)
 
-    # answers = quickSort(points,0,(numGen - 1))
 
-    for i in range(len(answers)):
-        print("X: ", answers[i][0], " Y: ", answers[i][1], "\n")
+    # for i in range(len(answers)):
+    #     print("X: ", answers[i][0], " Y: ", answers[i][1], "\n")
 
     print("Input: ", numGen, "Output: ", len(answers),"\n")
+
+    # isRight = checkHull(answers, points)
+    # if not isRight:
+    #     print("Error!")
 
     return 0
 
